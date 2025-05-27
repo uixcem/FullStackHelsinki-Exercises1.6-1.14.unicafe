566088d4b9147fe3c1fab281d4be1235a865c4d3
Cem <uixcem@Cems-iMac.local>
2025-05-28 00:09:22 +0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FullStackHelsinki-Exercises1.6-1.14.unicafe/',
   plugins: [react()],
+  base: '/FullStackHelsinki-Exercises1.6-1.14/unicafe/'
 })
